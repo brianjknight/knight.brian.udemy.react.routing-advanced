695747d429d9f9dd6970c93383349c80e884de0f
Brian Knight <knight.j.brian@gmail.com>
2025-05-07 11:56:54 -0600

    completed #371

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,14 +1,25 @@
-import { useParams } from "react-router-dom";
+import { useRouteLoaderData } from "react-router-dom";
+
+import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-  const params = useParams();
+  const data = useRouteLoaderData("event-detail");
 
-  return (
-    <>
-      <h1>EventDetailPage</h1>
-      <p>Event ID: {params.eventId}</p>
-    </>
-  );
+  return <EventItem event={data.event} />;
 }
 
 export default EventDetailPage;
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+
+  const response = await fetch("http://localhost:8080/events/" + id);
+
+  if (!response.ok) {
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    });
+  } else {
+    return response;
+  }
+}
